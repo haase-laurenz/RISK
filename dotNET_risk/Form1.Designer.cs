16393020993d55306afe912b3501113591a23037
KingLauri200 <laurenz.haase@gmx.de>
2024-04-28 10:59:54 +0200

    init .net

--- dotNET_risk/Form1.Designer.cs
+++ dotNET_risk/Form1.Designer.cs
@@ -28,13 +28,39 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.components = new System.ComponentModel.Container();
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.pictureBox1.Location = new System.Drawing.Point(349, 67);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(445, 511);
+			this.pictureBox1.TabIndex = 0;
+			this.pictureBox1.TabStop = false;
+			this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.ClientSize = new System.Drawing.Size(1141, 637);
+			this.Controls.Add(this.pictureBox1);
+			this.Name = "Form1";
 			this.Text = "Form1";
+			this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			this.ResumeLayout(false);
+
 		}
 
 		#endregion
+
+		private System.Windows.Forms.PictureBox pictureBox1;
 	}
 }
 
--- dotNET_risk/Form1.cs
+++ dotNET_risk/Form1.cs
@@ -7,14 +7,51 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Svg;
+
 
 namespace dotNET_risk
 {
 	public partial class Form1 : Form
 	{
+
+		Bitmap bitmap = null;
+
 		public Form1()
 		{
 			InitializeComponent();
+			string svgFilePath = "C:/Users/Laurenz Haase/Meine_Projekte/RISK/RISK/dotNET_risk/ressources/Risk_board.svg"; // Passe den Pfad zur SVG-Datei entsprechend deinem Projekt an
+
+			SvgDocument svgDocument = SvgDocument.Open(svgFilePath);
+
+			if (svgDocument != null)
+			{
+				Bitmap originalBitmap = svgDocument.Draw();
+
+				// Neue Breite und Höhe für das skalierte Bild
+				int newWidth = 1000; // Neue Breite in Pixel
+				int newHeight = (int)((double)newWidth / originalBitmap.Width * originalBitmap.Height); // Berechnung der Höhe basierend auf dem Seitenverhältnis
+
+				// Erstelle ein neues Bitmap mit der skalierten Größe
+				Bitmap resizedBitmap = new Bitmap(originalBitmap, newWidth, newHeight);
+
+				// Verwende das skalierte Bitmap
+				this.bitmap = resizedBitmap;
+
+			}
+		}
+
+		private void pictureBox1_Load(object sender, EventArgs e)
+		{
+			
+		}
+
+		private void pictureBox1_Paint(object sender, PaintEventArgs e)
+		{
+			pictureBox1.Image = bitmap;
 		}
 	}
+
+	
+
 }
